630f2737258c8d73656bcad8e2f62881056670a4
Tony Ward <tonygward@googlemail.com>
2020-08-04 17:05:06 +0100

    Removed duplicate foot/men js

--- biog/chrisfogg/layout/footer.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.get({
-        url: "../../shared/footer.html",
-        cache: false
-    }).then(function(data) {
-        $("#footer").html(data);
-    });
-});
--- shows/tommy/layout/menu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.get({
-        url: "../../shared/menu.html",
-        cache: false
-    }).then(function(data) {
-        $("#menu").html(data);
-    });
-});
